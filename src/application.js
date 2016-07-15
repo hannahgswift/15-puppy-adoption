31e333c30895cfb2f8a808a5d4e10b9eef9377a9
Hannah Swift <h.graceswift@gmail.com>
2016-07-15 13:33:04 -0500

    New puppy card appears, although jank, it is there

--- src/application.js
+++ src/application.js
@@ -2,21 +2,47 @@ export default class ApplicationView {
   constructor(appElement) {
     this.appElement = appElement;
     this.data = [];
+    console.log(this.appElement);
 
-    this.element = document.querySelector('.puppy-list');
+    this.element = document.createElement('li');
+    this.element.classList.add('puppy-list__item');
+    this.element.innerHTML = `<div class="puppy-card">
+      <figure class="pic-frame">
+        <img src="" alt="" class="pic-frame__pic">
+      </figure>
+      <div class="card-info">
+        <ul class="card-list">
+          <li class="card-list__item card-list__name"></li>
+          <li class="card-list__item card-list__age"></li>
+          <li class="card-list__item card-list__photourl"></li>
+          <li class="card-list__item card-list__profile"></li>
+        </ul>
+        <div class="button-container">
+          <button class="btn btn-dlt">Delete</button>
+          <button class="btn dtn-udt">Update</button>
+        </div>
+      </div>
+    </div>`;
+
+
   }
 
   render() {
-    this.element.innerHTML = '';
-    console.log('render is here');
+    // this.element.innerHTML = '';
+    console.log(this.appElement);
+
+    this.appElement.appendChild(this.element);
+
+    this.appElement.querySelector('.card-list__name').innerText = 'Fido';
+
   }
 
   start() {
     return fetch(`http://tiny-tn.herokuapp.com/collections/ryan-puppy`)
-    .then((res) => res.json())
-    .then((data) => {
-
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   }
 
 }
