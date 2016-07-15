8dd2312c86869f68d42a250564b1d7217ca1f9f7
Hannah Swift <h.graceswift@gmail.com>
2016-07-15 16:32:10 -0500

    List of puppy cards appear

--- src/application.js
+++ src/application.js
@@ -1,47 +1,28 @@
+import PuppyView from 'puppy-view';
+
 export default class ApplicationView {
   constructor(appElement) {
     this.appElement = appElement;
     this.data = [];
-    console.log(this.appElement);
-
-    this.element = document.createElement('li');
-    this.element.classList.add('puppy-list__item');
-    this.element.innerHTML = `<div class="puppy-card">
-      <figure class="pic-frame">
-        <img src="" alt="" class="pic-frame__pic">
-      </figure>
-      <div class="card-info">
-        <ul class="card-list">
-          <li class="card-list__item card-list__name"></li>
-          <li class="card-list__item card-list__age"></li>
-          <li class="card-list__item card-list__photourl"></li>
-          <li class="card-list__item card-list__profile"></li>
-        </ul>
-        <div class="button-container">
-          <button class="btn btn-dlt">Delete</button>
-          <button class="btn dtn-udt">Update</button>
-        </div>
-      </div>
-    </div>`;
-
-
   }
 
-  render() {
-    // this.element.innerHTML = '';
-    console.log(this.appElement);
+  render(data) {
+    this.appElement.innerHTML = '';
 
-    this.appElement.appendChild(this.element);
+    const components = this.data.map((item) => new PuppyView(item));
 
-    this.appElement.querySelector('.card-list__name').innerText = 'Fido';
-
+    components.forEach((card) => {
+      this.appElement.appendChild(card.element);
+      card.render();
+    })
   }
 
   start() {
     return fetch(`http://tiny-tn.herokuapp.com/collections/ryan-puppy`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        this.data = data;
+        this.render(data);
       });
   }
 
--- src/puppy-view.js
+++ src/puppy-view.js
@@ -1,6 +1,57 @@
 
 export default class PuppyView {
   constructor(currentPup, app) {
+    this.currentPup = currentPup;
+
+    console.log(this.element);
+
+    this.element = document.createElement('div');
+    this.element.classList.add('puppy-list__item');
+    this.element.innerHTML = `
+    <div class="container">
+      <div class="puppy-list">
+        <div class="puppy-card">
+          <figure class="pic-frame">
+            <img src="http://placekitten.com/300" alt="" class="pic-frame__pic">
+          </figure>
+          <div class="card-info">
+            <ul class="card-list">
+              <li class="card-list__item card-list__name">
+                <p class="card-info__title">Name</p>
+                <input class="card-input" type="text">
+              </li>
+              <li class="card-list__item card-list__age">
+                <p class="card-info__title">Age</p>
+                <input class="card-input" type="text">
+              </li>
+              <li class="card-list__item card-list__photourl">
+                <p class="card-info__title">Photo URL</p>
+                <input class="card-input" type="text">
+              </li>
+              <li class="card-list__item card-list__profile">
+                <p class="card-info__title">Profile</p>
+                <input class="card-input" type="text">
+              </li>
+            </ul>
+            <div class="button-container">
+              <button class="btn btn-dlt">Delete</button>
+              <button class="btn dtn-udt">Update</button>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>`;
+
+    console.log('puppy view present for pupppy duty');
+  }
+
+  render() {
+    console.log(this.element);
+
+    // this.appElement.appendChild(this.element);
 
   }
+
 }
+
+// console.log('friday is lit');
